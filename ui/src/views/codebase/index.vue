5ca431c72f989b79718fb4a6b5d62204546d3c15
wenchao <wenchao@ndsec.work>
2023-04-23 16:34:48 +0800

    init editor

--- ui/src/views/codebase/index.vue
+++ ui/src/views/codebase/index.vue
@@ -1,26 +1,53 @@
 <!--  -->
 <template>
-    <div class="">hello</div>
+    <div class="">
+        <el-button @click="changeLanguage('javascript')">js</el-button>
+        <el-button @click="changeLanguage('java')">java</el-button>
+        <div id="editor" style="width: 500px; height: 500px"></div>
+    </div>
 </template>
 
 <script>
+import loader from '@monaco-editor/loader'
 export default {
     components: {},
     data() {
-        return {}
+        return {
+            language: 'javascript',
+            monacoeditor: undefined,
+        }
     },
     computed: {},
     watch: {},
-    created() {},
-    mounted() {},
-    beforeCreate() {},
-    beforeMount() {},
-    beforeUpdate() {},
-    updated() {},
-    beforeDestroy() {},
-    destroyed() {},
-    activated() {},
-    methods: {},
+    async created() {
+        this.init()
+    },
+
+    methods: {
+        async init() {
+            loader.init().then((monaco) => {
+                const editorOptions = {
+                    language: 'java',
+                    minimap: { enabled: true },
+                }
+                var monacoeditor = monaco.editor
+                var standaloneeditor = monacoeditor.create(
+                    document.getElementById('editor'),
+                    editorOptions,
+                )
+
+                this.standaloneeditor = standaloneeditor
+
+                this.monacoeditor = monacoeditor
+            })
+        },
+        changeLanguage(lang) {
+            this.monacoeditor.setModelLanguage(
+                this.standaloneeditor.getModel(),
+                lang,
+            )
+        },
+    },
 }
 </script>
 <style lang="scss" scoped></style>
--- ui/vue.config.js
+++ ui/vue.config.js
@@ -1,4 +1,6 @@
 'use strict'
+
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 const path = require('path')
 const defaultSettings = require('./src/settings.js')
 
@@ -47,7 +49,11 @@ module.exports = {
             alias: {
                 '@': resolve('src')
             }
-        }
+        },
+        plugins: [
+            new MonacoWebpackPlugin({
+            }),
+        ],
     },
     chainWebpack(config) {
         // it can improve the speed of the first screen, it is recommended to turn on preload
@@ -82,6 +88,13 @@ module.exports = {
             })
             .end()
 
+        config.resolve.alias.set(
+            'vscode',
+            path.resolve(
+                './node_modules/monaco-languageclient/lib/vscode-compatibility'
+            )
+        );
+
         config
             .when(process.env.NODE_ENV !== 'development',
                 config => {
@@ -95,28 +108,28 @@ module.exports = {
                         .end()
                     config
                         .optimization.splitChunks({
-                        chunks: 'all',
-                        cacheGroups: {
-                            libs: {
-                                name: 'chunk-libs',
-                                test: /[\\/]node_modules[\\/]/,
-                                priority: 10,
-                                chunks: 'initial' // only package third parties that are initially dependent
-                            },
-                            elementUI: {
-                                name: 'chunk-elementUI', // split elementUI into a single package
-                                priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
-                                test: /[\\/]node_modules[\\/]_?element-ui(.*)/ // in order to adapt to cnpm
-                            },
-                            commons: {
-                                name: 'chunk-commons',
-                                test: resolve('src/components'), // can customize your rules
-                                minChunks: 3, //  minimum common number
-                                priority: 5,
-                                reuseExistingChunk: true
+                            chunks: 'all',
+                            cacheGroups: {
+                                libs: {
+                                    name: 'chunk-libs',
+                                    test: /[\\/]node_modules[\\/]/,
+                                    priority: 10,
+                                    chunks: 'initial' // only package third parties that are initially dependent
+                                },
+                                elementUI: {
+                                    name: 'chunk-elementUI', // split elementUI into a single package
+                                    priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
+                                    test: /[\\/]node_modules[\\/]_?element-ui(.*)/ // in order to adapt to cnpm
+                                },
+                                commons: {
+                                    name: 'chunk-commons',
+                                    test: resolve('src/components'), // can customize your rules
+                                    minChunks: 3, //  minimum common number
+                                    priority: 5,
+                                    reuseExistingChunk: true
+                                }
                             }
-                        }
-                    })
+                        })
                     // https:// webpack.js.org/configuration/optimization/#optimizationruntimechunk
                     config.optimization.runtimeChunk('single')
                 }
